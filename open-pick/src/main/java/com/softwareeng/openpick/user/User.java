d462241e70fb5e2fb90e271b3439199340c7cadf
zaraantonia <zaraantonia@yahoo.com>
2021-12-24 02:06:55 +0200

    min 55 from tutorial

--- open-pick/src/main/java/com/softwareeng/openpick/user/User.java
+++ open-pick/src/main/java/com/softwareeng/openpick/user/User.java
@@ -62,4 +62,15 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- open-pick/src/test/java/com/softwareeng/openpick/UserRepositoryTests.java
+++ open-pick/src/test/java/com/softwareeng/openpick/UserRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
@@ -18,14 +20,55 @@ public class UserRepositoryTests {
     @Test
     public void testAddNew(){
         User user = new User();
-        user.setEmail("test@test.com");
+        user.setEmail("test2@test.com");
         user.setPassword("test");
-        user.setFirstName("Gheorghe");
-        user.setLastName("Elena");
+        user.setFirstName("Traistariu");
+        user.setLastName("Mihai");
 
         User savedUser = repo.save(user);
 
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll(){
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+        for (User user : users) {
+            System.out.println(user.toString());
+
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("hello2000");
+        repo.save(user);
+
+        User updatedUser = repo.findById(userId).get();
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("hello2000");
+    }
+
+    @Test
+    public void testGet(){
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+
+        Assertions.assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+    }
+
+    @Test
+    public void testDelete(){
+        Integer userId = 2;
+        repo.deleteById(userId);
+
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
     }
 }
